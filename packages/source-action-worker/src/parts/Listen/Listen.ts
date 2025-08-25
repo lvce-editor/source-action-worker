9026756f70232aab8a110148560370c203218ccd
Le Vivilet <levivlet@pm.me>
2025-08-25 11:43:38 +0200

    feature: register commands

--- packages/source-action-worker/src/parts/Listen/Listen.ts
+++ packages/source-action-worker/src/parts/Listen/Listen.ts
@@ -1,8 +1,10 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
+import { registerCommands } from '../SourceActionStates/SourceActionStates.ts'
 
 export const listen = async (): Promise<void> => {
+  registerCommands(CommandMap.commandMap)
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
