04be3d71b4ba9f7395f4880f4b82f47b106bb2c0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-25 23:03:33 +0200

    feature: implement organizeImport function (#24)

--- packages/source-action-worker/src/parts/SelectItem/SelectItem.ts
+++ packages/source-action-worker/src/parts/SelectItem/SelectItem.ts
@@ -1,5 +1,14 @@
 import type { SourceActionState } from '../SourceActionState/SourceActionState.ts'
+import * as ApplyEdit from '../ApplyEdit/ApplyEdit.ts'
+import * as Close from '../Close/Close.ts'
+import * as GetEdits from '../GetEdits/GetEdits.ts'
+import * as SourceActionName from '../SourceActionName/SourceActionName.ts'
 
 export const selectItem = async (state: SourceActionState, name: string): Promise<SourceActionState> => {
-  return state
+  const { editorUid } = state
+  if (name === SourceActionName.OrganizeImports) {
+    const edits = await GetEdits.getEdits(editorUid)
+    await ApplyEdit.applyEdit(editorUid, edits)
+  }
+  return Close.close(state)
 }
