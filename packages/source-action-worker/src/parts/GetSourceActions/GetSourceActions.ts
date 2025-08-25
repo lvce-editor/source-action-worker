0487b0be5045ebbb88f4f4953551c65e825779fa
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-25 16:31:10 +0200

    feature: update command id (#16)

--- packages/source-action-worker/src/parts/GetSourceActions/GetSourceActions.ts
+++ packages/source-action-worker/src/parts/GetSourceActions/GetSourceActions.ts
@@ -4,6 +4,6 @@ import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
 // TODO ask extension host worker instead
 export const getEditorSourceActions = async (editorId: number): Promise<readonly SourceActionItem[]> => {
   // @ts-ignore
-  const sourceActions = await EditorWorker.invoke('Editor.getEditorSourceActions', editorId)
+  const sourceActions = await EditorWorker.invoke('Editor.getSourceActions', editorId)
   return []
 }
