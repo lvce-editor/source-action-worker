8f9506574dbf50897a151bd46948e3824433fed8
Le Vivilet <levivlet@pm.me>
2025-08-25 23:02:26 +0200

    feature: improve applyEdit

--- packages/source-action-worker/src/parts/ApplyEdit/ApplyEdit.ts
+++ packages/source-action-worker/src/parts/ApplyEdit/ApplyEdit.ts
@@ -2,5 +2,6 @@ import type { Change } from '../Change/Change.ts'
 import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
 
 export const applyEdit = async (editorUid: number, changes: readonly Change[]): Promise<void> => {
-  await EditorWorker.applyEdit(editorUid, changes)
+  // @ts-ignore
+  await EditorWorker.invoke('Editor.applyDocumentEdits', editorUid, changes)
 }
