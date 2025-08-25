161d4100bd88b58f91ea40c53d2f5e82c0715dfe
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-25 23:04:16 +0200

    fix: close command (#22)
    * fix: close command
    
    * test

--- packages/source-action-worker/src/parts/WidgetId/WidgetId.ts
+++ packages/source-action-worker/src/parts/WidgetId/WidgetId.ts
@@ -1,1 +1,8 @@
+export const CodeGenerator = 1
+export const ColorPicker = 2
 export const Completion = 3
+export const CompletionDetail = 4
+export const Find = 5
+export const Hover = 6
+export const Rename = 7
+export const SourceAction = 8
