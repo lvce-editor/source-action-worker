d30a29cba34fbf07358127a082125e174b0d1640
Le Vivilet <levivilet@pm.me>
2026-07-18 11:12:39 +0200

    feature: apply source action edits (#36)
    * feature: apply source action edits
    
    * fix: type extension host invoke mock

--- packages/source-action-worker/src/parts/Change/Change.ts
+++ packages/source-action-worker/src/parts/Change/Change.ts
@@ -1,17 +1,5 @@
-export interface Range {
-  readonly columnIndex: number
-  readonly rowIndex: number
-}
-
-export interface Selection {
-  readonly end: Range
-  readonly start: Range
-}
-
 export interface Change {
-  readonly deleted: readonly string[]
-  readonly end: Range
-  readonly inserted: readonly string[]
-  readonly origin: string
-  readonly start: Range
+  readonly endOffset: number
+  readonly inserted: string
+  readonly startOffset: number
 }
--- packages/source-action-worker/src/parts/SourceActionItem/SourceActionItem.ts
+++ packages/source-action-worker/src/parts/SourceActionItem/SourceActionItem.ts
@@ -1,4 +1,7 @@
+import type { Change } from '../Change/Change.ts'
+
 export interface SourceActionItem {
+  readonly edits?: readonly Change[]
   readonly isFocused: boolean
   readonly name: string
 }
